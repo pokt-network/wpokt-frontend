bb8b0948456fb9e1328cc83c555ccb66c290ae45
geovgy <54918343+geovgy@users.noreply.github.com>
2025-06-03 09:24:24 -0400

    Add a .env variable for disabling bridge during pause

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 import { Bridge } from '@/components/Bridge'
+import { IS_PAUSED } from '@/utils/constants'
 
 
 export default function Home() {
   return (
     <Flex direction="column" minHeight="100vh" overflowX="hidden">
       <Header />
+      {IS_PAUSED && (
+        <Flex 
+          flexGrow={1} 
+          justify="center" 
+          align="center" 
+          paddingY={10}
+          bg="poktLime"
+          color="darkBlue"
+          width="100%"
+        >
+          <Text fontSize="lg" fontWeight="bold">
+            Bridge will be paused for 24 hours starting at 10am ET today (June 3rd)
+          </Text>
+        </Flex>
+      )}
       <Flex flexGrow={1} justify="center" align="center" paddingY={10}>
         <Bridge />
       </Flex>
