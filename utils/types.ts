54e3a1024bf4f365baba3bcd91ff07f78402ee30
geovgy <54918343+geovgy@users.noreply.github.com>
2025-11-28 13:01:13 -0500

    Declare @cosmjs/stargate as module

--- utils/types.ts
+++ utils/types.ts
@@ -4,7 +4,7 @@ export type SupportedPoktChain = "testnet" | "mainnet" | "pocket-beta";
 
 export interface SignedPocketShannonTransaction {
   fee: string
-  rawTx: string
-  signature: string
-  transactionHex: string
+rawTx: string
+signature: string
+transactionHex: string
 }
